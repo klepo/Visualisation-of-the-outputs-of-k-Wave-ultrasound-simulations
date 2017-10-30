934e56d44a3ccea0fcdb319ba6562f76c2aa0ace
Petr Klepárník <ikleparnik@fit.vutbr.cz>
2017-10-30 08:49:49 +0100

    doc

--- projects/k-wave-h5-processing/settings.cpp
+++ projects/k-wave-h5-processing/settings.cpp
@@ -11,7 +11,7 @@
  *              created by the k-Wave toolbox - http://www.k-wave.org. This file may be used,
  *              distributed and modified under the terms of the LGPL version 3 open source
  *              license. A copy of the LGPL license should have been received with this file.
- *              Otherwise, it can be found at", "http://www.gnu.org/copyleft/lesser.html.
+ *              Otherwise, it can be found at: http://www.gnu.org/copyleft/lesser.html.
  *
  * @copyright   Copyright © 2017, Petr Kleparnik, VUT FIT Brno. All Rights Reserved.
  *
