c2955b2669b0d89296fd26ab00f962e0cc328b74
Petr Klepárník <ikleparnik@fit.vutbr.cz>
2018-10-23 18:41:53 +0200

    data modified

--- projects/k-wave-h5-helper-parallel/k-wave-h5-helper-parallel.h
+++ projects/k-wave-h5-helper-parallel/k-wave-h5-helper-parallel.h
@@ -1,16 +1,20 @@
-﻿/*
- * @file        hdf5helperparallel.h
- * @author      Petr Kleparnik, VUT FIT Brno, xklepa01@stud.fit.vutbr.cz
- * @version     0.0
- * @date        30 July 2014
+﻿/**
+ * @file        k-wave-h5-helper-parallel.h
+ * @author      Petr Kleparnik, VUT FIT Brno, ikleparnik@fit.vutbr.cz
+ * @version     1.1
+ * @date        30 July      2014 (created) <br>
+ *              23 October   2018 (updated)
  *
  * @brief       Old not updated nonfunctional! The header file including important HDF5Helper classes with defined PARALLEL_HDF5 macro.
  *
- * @section     Licence
- * This file is part of hdf5file library for k-Wave h5 processing
- * for preprocessing HDF5 data created by the k-Wave toolbox - http://www.k-wave.org.
- * Copyright © 2014, Petr Kleparnik, VUT FIT Brno.
- * hdf5file library is free software.
+ * @license     This file is part of the k-wave-h5-helper library for processing the HDF5 data
+ *              created by the k-Wave toolbox - http://www.k-wave.org. This file may be used,
+ *              distributed and modified under the terms of the LGPL version 3 open source
+ *              license. A copy of the LGPL license should have been received with this file.
+ *              Otherwise, it can be found at: http://www.gnu.org/copyleft/lesser.html.
+ *
+ * @copyright   Copyright © 2018, Petr Kleparnik, VUT FIT Brno. All Rights Reserved.
+ *
  */
 
 #ifndef HDF5HELPERPARALLEL_H
