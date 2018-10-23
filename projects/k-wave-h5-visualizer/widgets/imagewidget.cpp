4c24a9349af6a03890d696d9d9a9623eabf4a8df
Petr Klepárník <ikleparnik@fit.vutbr.cz>
2018-10-23 18:51:53 +0200

    date modified

--- projects/k-wave-h5-visualizer/widgets/imagewidget.cpp
+++ projects/k-wave-h5-visualizer/widgets/imagewidget.cpp
@@ -3,7 +3,7 @@
  * @author      Petr Kleparnik, VUT FIT Brno, ikleparnik@fit.vutbr.cz
  * @version     1.1
  * @date        30 July      2014 (created) <br>
- *              9  October   2018 (updated)
+ *              10 October   2018 (updated)
  *
  * @brief       The implementation file containing ImageWidget class definition.
  *
--- projects/k-wave-h5-visualizer/widgets/timeseriescontrolgroupbox.h
+++ projects/k-wave-h5-visualizer/widgets/timeseriescontrolgroupbox.h
@@ -3,7 +3,7 @@
  * @author      Petr Kleparnik, VUT FIT Brno, ikleparnik@fit.vutbr.cz
  * @version     1.1
  * @date        9  October   2018 (created) <br>
- *              9  October   2018 (updated)
+ *              10 October   2018 (updated)
  *
  * @brief       The header file with TimeSeriesControlGroupBox class declaration.
  *
